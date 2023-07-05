e25b5333c6998fe20dbb7a02177adecc17a13170
早点睡觉 <1921216468@qq.com>
2023-07-05 14:38:36 +0800

    创建通用数据获取钩子

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,40 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Genres {
   id: number;
   name: string;
 }
 
-interface FetchGenresResponse {
-  count: number;
-  results: Genres[];
-}
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genres[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const contrller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchGenresResponse>("/genres")
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => contrller.abort();
-  }, []);
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genres>("/genres");
 
 export default useGenres;
